88d4f0528796851bb783663b0f88a8d35e7f9136
Doğan Çelik <dogancelik93@gmail.com>
2014-07-14 02:23:07 +0300

    New script: KickassTorrents Skip Url Confirm

---
+++ kickasstorrents_skip_url_confirm.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        KickassTorrents Skip Url Confirm
+// @namespace   dogancelik.com
+// @include     https://kickass.to/confirm/url/*
+// @version     1.0
+// @grant       none
+// ==/UserScript==
+document.querySelector("center a").click();
