76367e42290c3bf97e19de1a1c031e69b6b300de
Doğan Çelik <dogancelik93@gmail.com>
2020-07-08 08:26:46 +0300

    Fix github-add-commented

--- github-add-commented.user.js
+++ github-add-commented.user.js
@@ -5,44 +5,48 @@
 // @include     https://github.com/issues/*
 // @include     https://github.com/issues?*
 // @include     https://github.com/issues
-// @version     1.0.1
+// @version     1.1.0
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
 // @updateURL   https://github.com/dogancelik/userscripts/raw/master/github-add-commented.user.js
 // @grant       none
 // ==/UserScript==
 
+/* eslint-env jquery */
+
 function getUsername () {
-  return document.querySelector('.header-nav-current-user .css-truncate-target').textContent.trim();
+	return document.querySelector('.header-nav-current-user .css-truncate-target').textContent.trim();
 }
 
 function getQuery () {
-  return 'is:open is:issue commenter:' + getUsername();
+	return 'is:open is:issue commenter:' + getUsername();
 }
 
 function isAdded (links) {
-  return links.innerHTML.indexOf('Commented') > -1;
+	return links.innerHTML.indexOf('Commented') > -1;
 }
 
 function add (links) {
-  var html = '<a href="/issues?q=::query::" aria-label="Issues commented by you" class="js-selected-navigation-item subnav-item" data-selected-links="dashboard_commented /issues?q=::query::" role="tab">Commented</a>';
-  var query = encodeURIComponent(getQuery());
-  if (window.location.search.indexOf(query) > -1) {
-    html = html.replace('js-selected-navigation-item', 'js-selected-navigation-item selected');
-  }
-  links.innerHTML = links.innerHTML + html.replace(/::query::/g, query);
+	var html = '<a href="/issues?q=::query::" aria-label="Issues commented by you" class="js-selected-navigation-item subnav-item" data-selected-links="dashboard_commented /issues?q=::query::" role="tab">Commented</a>';
+	var query = encodeURIComponent(getQuery());
+	if (window.location.search.indexOf(query) > -1) {
+		html = html.replace('js-selected-navigation-item', 'js-selected-navigation-item selected');
+	}
+	links.innerHTML = links.innerHTML + html.replace(/::query::/g, query);
 }
 
 function removeCommenter (i, e) {
-  var deleteQuery = encodeURIComponent('commenter:' + getUsername());
-  e.href = e.href.replace(deleteQuery,'');
+	var deleteQuery = encodeURIComponent('commenter:' + getUsername());
+	e.href = e.href.replace(deleteQuery,'');
 }
 
 function check () {
-  var links = document.querySelector('.subnav-links.left');
+	var links = document.querySelector('.subnav-links');
 
-  if (links != null && !isAdded(links)) {
-    add(links);
-    $(links).find('a').on('click', check).slice(0, 3).each(removeCommenter);
-  } else setTimeout(check, 50);
+	if (links != null && !isAdded(links)) {
+		add(links);
+		$(links).find('a').on('click', check).slice(0, 3).each(removeCommenter);
+	} else setTimeout(check, 50);
 }
 
+$('#js-pjax-container').on('pjax:end', check);
 check();
