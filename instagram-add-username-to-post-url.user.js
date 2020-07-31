539dc5bb6579dca6bf34c7d3184501154fe4b70c
Doğan Çelik <dogancelik93@gmail.com>
2020-07-31 21:16:27 +0300

    Update instagram-add-username-to-post-url
    Use pushState, avoid reload

--- instagram-add-username-to-post-url.user.js
+++ instagram-add-username-to-post-url.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Instagram Add Username to Post URL
 // @namespace    dogancelik.com
-// @version      0.3.0
+// @version      0.4.0
 // @description  Add username to post URL
 // @author       Doğan Çelik
 // @match        https://www.instagram.com/p/*
@@ -9,16 +9,18 @@
 // ==/UserScript==
 
 if (location.pathname.indexOf('/p/') === 0) {
-  var data = {};
-  var keys = Object.keys(window.__additionalData);
-  if (keys.length > 0) {
-    data = window.__additionalData[keys[0]].data;
-  } else if (window._sharedData && window._sharedData.hasOwnProperty('entry_data')) {
-    data = window._sharedData.entry_data.PostPage[0];
-  } else {
-    throw new Error("No user data");
-  }
+	var data = {};
+	var keys = Object.keys(window.__additionalData);
+	if (keys.length > 0) {
+		data = window.__additionalData[keys[0]].data;
+	} else if (window._sharedData && window._sharedData.hasOwnProperty('entry_data')) {
+		data = window._sharedData.entry_data.PostPage[0];
+	} else {
+		throw new Error("No user data");
+	}
 
-  var username = data.graphql.shortcode_media.owner.username;
-  window.location.pathname = '/' + username + window.location.pathname;
+	var username = data.graphql.shortcode_media.owner.username,
+		newPathname = '/' + username + window.location.pathname;
+
+	history.pushState(undefined, undefined, newPathname);
 }
