e412f2fa1cf29ae246e85297e02170cc27d090d1
Doğan Çelik <dogancelik93@gmail.com>
2020-11-01 03:19:20 +0300

    Update archive-today-quick-copy-info
    * Copy 4chan OP text & date

--- archive-today-quick-copy-info.user.js
+++ archive-today-quick-copy-info.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Archive.today Quick Copy Info
 // @namespace    dogancelik.com
-// @version      0.1.0
+// @version      0.2.0
 // @description  Adds buttons for copying saved URL, archived URL, page title & tweet text
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
 // @match        https://archive.today/*
@@ -37,21 +37,37 @@ function extractData() {
 	data.wiki.archivedate = data.wiki.archivedate.replace(' }}', '');
 }
 
-function isTweet() {
-	return data.wiki.url.indexOf('twitter.com') > -1 ||
-		data.wiki.url.indexOf('threadreaderapp.com') > -1;
-}
-
-function getTweetText() {
-	let tweet;
-	if (tweet = document.querySelector('div[old-class="js-tweet-text-container"]')) {
-		return tweet.textContent.trim();
-	} else if (tweet = document.querySelector('div[id="tweet_1"]')) {
-		return tweet.textContent.replace('\uF0C1', '').trim();
-	} else {
-		return undefined;
+function getContextData(site, get) {
+	if (site === 'twi') {
+		if (get === 'is') {
+			return data.wiki.url.indexOf('twitter.com') > -1 ||
+				data.wiki.url.indexOf('threadreaderapp.com') > -1;
+		} else if (get === 'text') {
+			let tweet;
+			if (tweet = document.querySelector('div[old-class="js-tweet-text-container"]')) {
+				return tweet.textContent.trim();
+			} else if (tweet = document.querySelector('div[id="tweet_1"]')) {
+				return tweet.textContent.replace('\uF0C1', '').trim();
+			} else {
+				return undefined;
+			}
+		}
+	} else if (site === '4ch') {
+		if (get === 'is') {
+			return data.wiki.url.indexOf('4chan.org') > -1 ||
+				data.wiki.url.indexOf('4channel.org') > -1;
+		} else if (get === 'text') {
+			let subject = document.querySelector('[old-class*="opContainer"] [old-class~="postInfo"][old-class~="desktop"] [old-class*="subject"]').innerText,
+				message = document.querySelector('[old-class*="opContainer"] [old-class*="postMessage"]').innerText;
+			return subject + ' ' + message;
+		} else if (get === 'date') {
+			let date = document.querySelector('[old-class*="opContainer"] [old-class="dateTime"]').innerText;
+			date = new Date(date);
+			return date.toISOString();
+		}
 	}
 }
+
 
 function clip(event, text) {
 	let target = $(event.delegateTarget),
@@ -74,7 +90,10 @@ if (header.length > 0) {
 	<button id="copy-saved-url">Page URL</button>
 	<button id="copy-page-title">Page Title</button>
 	<button id="copy-archive-date">Archive Date</button>
-	${isTweet() ? '<button id="copy-tweet-text">Tweet Text</button>' : ''}
+	<span style="padding: 0 10px;">|</span>
+	${getContextData('twi', 'is') ? '<button id="copy-tweet-text">Tweet Text</button>' : ''}
+	${getContextData('4ch', 'is') ? '<button id="copy-4chan-text">OP Text</button>' : ''}
+	${getContextData('4ch', 'is') ? '<button id="copy-4chan-date">OP Date</button>' : ''}
 	<span style="padding: 0 10px;">|</span>
 	<button id="copy-archived-url-long">Archived Link (Long)</button>
 	<button id="copy-markdown">Markdown</button>
@@ -86,7 +105,10 @@ if (header.length > 0) {
 	qci.find('#copy-saved-url').on('click', (e) => clip(e, data.wiki.url));
 	qci.find('#copy-page-title').on('click', (e) => clip(e, data.wiki.title));
 	qci.find('#copy-archive-date').on('click', (e) => clip(e, data.wiki.archivedate));
-	qci.find('#copy-tweet-text').on('click', (e) => clip(e, getTweetText()));
+
+	qci.find('#copy-tweet-text').on('click', (e) => clip(e, getContextData('twi', 'text')));
+	qci.find('#copy-4chan-text').on('click', (e) => clip(e, getContextData('4ch', 'text')));
+	qci.find('#copy-4chan-date').on('click', (e) => clip(e, getContextData('4ch', 'date')));
 
 	qci.find('#copy-archived-url-long').on('click', (e) => clip(e, data.share.longLink));
 	qci.find('#copy-markdown').on('click', (e) => clip(e, data.share.markdown));
