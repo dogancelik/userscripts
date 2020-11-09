596b5818099aa4ff7edd534cfbeb5d9d711058c9
Doğan Çelik <dogancelik93@gmail.com>
2020-11-10 01:52:57 +0300

    Revert: Greasemonkey 4 support for embedupload-fix
    This reverts commit da0746e01490801474614472ad4f7180d9933b66.

--- embedupload-fix.user.js
+++ embedupload-fix.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name        EmbedUpload Fix
-// @description shows links without opening a new tab
+// @description Shows links without opening a new tab
 // @namespace   dogancelik.com
-// @include     http*://www.embedupload.com/?d=*
-// @include     http*://embedupload.com/?d=*
-// @version     2.0
+// @include     *://www.embedupload.com/?d=*
+// @include     *://embedupload.com/?d=*
+// @version     1.1
 // @require     https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
 // @grant       GM_addStyle
 // @grant       GM_getValue
@@ -19,80 +19,75 @@
 // ==/UserScript==
 
 function getReveal() {
-  return GM.getValue('reveal');
+	return GM_getValue('reveal') || 0;
 }
 
-getReveal().then(val => {
-  if (val === undefined)
-    GM.setValue('reveal', 0);
+var reveal = getReveal();
+
+GM_registerMenuCommand('Reveal links by click', function() {
+	GM_setValue('reveal', 0);
 });
 
-GM.registerMenuCommand("Reveal links by click", function() {
-  GM.setValue("reveal", 0);
+GM_registerMenuCommand('Reveal all links on load', function() {
+	GM_setValue('reveal', 1);
+	loopLinks(getLink);
 });
 
-GM.registerMenuCommand("Reveal all links on load", function() {
-  GM.setValue("reveal", 1);
-});
-
-GM.addStyle(".DownloadNowFix { font-size: 12px; font-style: normal; }");
-
-var exceptions = ['?MF'],
-    split_first = "You should copy/paste the download link on a new browser window : ",
-    split_last = "</b>";
+GM_addStyle('.DownloadNowFix { font-size: 12px; font-style: normal; }');
 
 function getLink(link) {
-  GM.xmlHttpRequest({
-    url: link.href,
-    method: "GET",
-    onload: function(res) {
-      var data = res.responseText;
+	let exceptions = ['?MF'],
+		split_first = 'You should copy/paste the download link on a new browser window : ',
+		split_last = '</b>';
 
-      var mirror_url,
-          is_exception;
-      
-      for (var i = 0; i < exceptions.length; i++) {
-        is_exception = link.href.indexOf(exceptions[i]) !== -1;
-      }
-      
-      if (is_exception) {
-        mirror_url = data.split(split_first)[1].split(split_last)[0].trim();
-      } else {
-        var container = document.createElement('div');
-        container.innerHTML = data;
-        mirror_url = container.querySelector("a[href^='http']").href;
-      }
+	function parsePage(res) {
+		let data = res.responseText,
+			mirror_url,
+			is_exception;
 
-      link.className = "DownloadNowFix";
-      link.target = "";
-      link.href = mirror_url;
-      link.innerHTML = mirror_url;
-    }
-  });
+		for (let i = 0; i < exceptions.length; i++) {
+			is_exception = link.href.indexOf(exceptions[i]) !== -1;
+		}
+
+		if (is_exception) {
+			mirror_url = data.split(split_first)[1].split(split_last)[0].trim();
+		} else {
+			let container = document.createElement('div');
+			container.innerHTML = data;
+			mirror_url = container.querySelector('a[href^="http"]').href;
+		}
+
+		link.className = 'DownloadNowFix';
+		link.target = '';
+		link.href = mirror_url;
+		link.innerHTML = mirror_url;
+	}
+
+	GM_xmlhttpRequest({
+		url: link.href,
+		method: 'GET',
+		onload: parsePage
+	});
 }
 
 function loopLinks(callback) {
-  var els = document.querySelectorAll(".DownloadNow");
-  for (var i = 0; i < els.length; i++) {
-    callback(els[i]);
-  }
+	let els = document.querySelectorAll('.DownloadNow');
+	for (let i = 0; i < els.length; i++) {
+		callback(els[i]);
+	}
 }
 
-getReveal().then(val => {
-if (val === 0) {
-  loopLinks(function(el) {
-    el.onclick = function(e) {
-      if (el.className !== "DownloadNowFix") {
-        getLink(el);
-        e.preventDefault();
-      }
-    };
-  });
-} else if (val === 1) {
-  window.onload = function() {
-    loopLinks(function(el) {
-      getLink(el);
-    });
-  };
+function revealOnClick(el) {
+	el.addEventListener('click', function(e) {
+		if (el.className !== 'DownloadNowFix') {
+			getLink(el);
+			e.preventDefault();
+		}
+	}, false);
 }
-});
+
+if (reveal === 0) {
+	loopLinks(revealOnClick);
+} else if (reveal === 1) {
+	loopLinks(getLink);
+}
