4fe4a3337dffc4b9621ef101bc0e814bb1ffa1cf
Doğan Çelik <dogancelik93@gmail.com>
2021-02-17 08:56:40 +0300

    Update archive-today-rewrite-wip

--- archive-today-rewrite-wip.user.js
+++ archive-today-rewrite-wip.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Archive.today Rewrite WIP
 // @namespace    dogancelik.com
-// @version      1.0.0
+// @version      1.1.0
 // @description  Visually removes "wip" from the URL
 // @match        https://archive.today/wip/*
 // @match        https://archive.is/wip/*
@@ -20,12 +20,16 @@ for (var i = 0; i < 10000; i++) window.clearInterval(i);
 let now = new Date(),
 	newPath = window.location.pathname.replace('/wip', ''),
 	isLoading = document.querySelector('img[src$="loading.gif"]'),
+	isInQueue = document.querySelector('img + span').textContent.indexOf('in queue') > -1,
 	getReqCount = (nodeList) => nodeList ? nodeList.length : 0,
 	allRequests = document.querySelectorAll('tr[valign="top"]:not([style="background-color:silver"])'),
 	finRequests = document.querySelectorAll('tr[valign="top"][style="background-color:#66FF66"]');
 
-if (isLoading) {
-	document.title = `Loading: ${getReqCount(finRequests)} / ${getReqCount(allRequests)}`;
+if (isInQueue) {
+	// document.title = 'In queue';
+} else if (isLoading) {
+	// document.title = `Loading: ${getReqCount(finRequests)} / ${getReqCount(allRequests)}`;
 }
 
-window.history.pushState(undefined, now.toString(), newPath);
+// window.history.pushState(undefined, now.toString(), newPath);
+console.log("Don't use this userscript, it erases the tab history.")
