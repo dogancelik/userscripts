9cb5161f859a04b4d56a31dfd82630bee8dbd3b0
Doğan Çelik <dogancelik93@gmail.com>
2022-07-18 05:11:43 +0300

    Update twitter-remove-popup

--- twitter-remove-popup.user.js
+++ twitter-remove-popup.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Twitter Remove Popup
 // @namespace    dogancelik.com
-// @version      0.3.1
+// @version      0.4.0
 // @description  Remove scroll blocking popup
 // @match        https://twitter.com/*
 // @grant        none
@@ -11,19 +11,25 @@
 
 /* eslint-env jquery, browser, es2020, greasemonkey */
 
+function isLoggedIn() {
+	return document.querySelector('div[aria-label="Account menu"]').length > 0;
+}
+
 const sheetDialog = 'div[data-testid="sheetDialog"]';
-document.arrive(`#layers ${sheetDialog} a[href="/login"]`, { existing: true }, function() {
+document.arrive(`#layers ${sheetDialog} a[href="/login"]`, { existing: true }, function () {
+	if (isLoggedIn()) return;
 	this.closest(sheetDialog).parentElement.remove();
 });
 
-document.arrive('#layers a[href="/i/flow/signup"]', { existing: true }, function() {
+document.arrive('#layers a[href="/i/flow/signup"]', { existing: true }, function () {
+	if (isLoggedIn()) return;
 	const last = $(this).parentsUntil('#layers').last();
 	if (last.find('div[aria-label="Image"]').length === 0) {
 		last.remove();
 	}
 });
 
-const observer = new MutationObserver(function() {
+const observer = new MutationObserver(function () {
 	document.firstElementChild.style.overflow = 'auto';
 	document.firstElementChild.style.overscrollBehaviorY = 'auto';
 });
